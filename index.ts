e1abd1c674e14ddfe3c51c8dbe095b837fae8224
wcxaaa <wcx@xtoneict.com>
2017-11-21 09:31:44 +0800

    * import component

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { InkbarModule } from './src/inkbar/inkbar.module';
+import { InkbarComponent } from './src/inkbar/inkbar.component';
 
 export { InkbarModule } from './src/inkbar/inkbar.module';
-
+export { InkbarComponent } from './src/inkbar/inkbar.component';
